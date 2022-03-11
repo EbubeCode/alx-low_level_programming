0f5fcf6d0bc359cfadb1a47a5329affbb66114fb
EbubeCode <chukwuma258@gmail.com>
2022-03-11 14:13:01 +0100

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,14 @@ int main(void)
 	i = 48;
 	while (i <= 57)
 	{
-		if (i == 57)
-			putchar(i);
-		else
+		putchar(i);
+		if (i != 57)
 		{
-			putchar(i);
 			putchar(44);
 			putchar(32);
 		}
 		i++;
 	}
+	putchar('\n');
 	return (0);
 }
