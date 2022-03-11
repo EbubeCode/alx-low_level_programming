0f8c875e4bc0899dca330a2ad5b3379742ef5c49
EbubeCode <chukwuma258@gmail.com>
2022-03-11 01:40:37 +0100

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 int main(void)
 {
 
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	
 	fwrite(str, 1, sizeof(str), stderr);
 	return (1);
 }
