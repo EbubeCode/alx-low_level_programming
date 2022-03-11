c11a0a905dc07ecfb34e972b4e2d34c92eaca625
EbubeCode <chukwuma258@gmail.com>
2022-03-11 13:14:59 +0100

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 97;
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	i = 65;
+	while (i <= 90)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
