30a1ec8350c53bdce4c636ebf55e915d07cd6ac3
EbubeCode <chukwuma258@gmail.com>
2022-03-15 14:06:21 +0100

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of n
+ * @n - the number to determine its sign
+ *
+ * Return: 0 if n = 0, 1 if n > 0, -1 if n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		retrun (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
