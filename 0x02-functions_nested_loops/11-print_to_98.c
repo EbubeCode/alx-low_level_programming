3eb077271913b5708dbcee07dc0819f67248d776
EbubeCode <chukwuma258@gmail.com>
2022-03-15 16:38:16 +0100

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - it prints numbers from n to 98 in order
+ * @n: this is the number that the printing should start from
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			if (n == 98)
+				printf("%d\n", n++);
+			else
+				printf("%d, ", n++);
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+				printf("%d\n", n++);
+			else
+				printf("%d, ", n++);
+		}
+	}
+}
