2c8945ff42429e9db6a4aad4a4f8d7b61f242482
EbubeCode <chukwuma258@gmail.com>
2022-03-15 16:41:21 +0100

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,9 +23,9 @@ void print_to_98(int n)
 		while (n >= 98)
 		{
 			if (n == 98)
-				printf("%d\n", n++);
+				printf("%d\n", n--);
 			else
-				printf("%d, ", n++);
+				printf("%d, ", n--);
 		}
 	}
 }
