fa71ab02ec05f4756c35fcece2ac59f93210675d
EbubeCode <chukwuma258@gmail.com>
2022-03-15 16:22:32 +0100

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - adds two integers n and x
+ * @n: this integer will be added to x
+ * @x: this integer will be added to n
+ *
+ * Return: the sum of n and x
+ */
+int add(int n, int x)
+{
+	return (n + x);
+}
