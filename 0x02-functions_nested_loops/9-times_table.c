ff070650295fc62f1385ddf906b7486d5bc858ec
EbubeCode <chukwuma258@gmail.com>
2022-03-15 16:12:13 +0100

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -47,10 +47,11 @@ void switch_print(int n)
  * seperate_print - seperates the mutiple b4 printing
  * @n: number to seperate
  * @x: to monitor printing
+ * @a: to monitor spacing
  *
  * Return: void
  */
-void seperate_print(int n, int x)
+void seperate_print(int n, int x, int a)
 {
 	int i;
 	int j;
@@ -59,7 +60,7 @@ void seperate_print(int n, int x)
 	j = (n / 10);
 	if (j > 0)
 		switch_print(j);
-	else if (j == 0 && i != 0)
+	else if (a == 1)
 		_putchar(' ');
 	switch_print(i);
 	if (x == 1)
@@ -88,9 +89,11 @@ void times_table(void)
 		{
 			k = i * j;
 			if (j == 9)
-				seperate_print(k, 0);
+				seperate_print(k, 0, 1);
+			else if (j > 0)
+				seperate_print(k, 1, 1);
 			else
-				seperate_print(k, 1);
+				seperate_print(k, 1, 0);
 			j++;
 		}
 		_putchar('\n');
