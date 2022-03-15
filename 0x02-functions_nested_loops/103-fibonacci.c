b71bddbc95d72d029cdb39868ae229b1e8fa2dc4
EbubeCode <chukwuma258@gmail.com>
2022-03-15 18:58:08 +0100

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long long int sum;
+	long int fib1;
+	long int fib2;
+	long int fib;
+
+	sum = 0;
+	fib1 = 1;
+	fib2 = 0;
+	fib = fib1 + fib2;
+	while (fib < 4000000)
+	{
+		if (fib % 2 == 0)
+			sum += fib;
+		fib2 = fib1;
+		fib1 = fib;
+		fib = fib1 + fib2;
+	}
+	printf("%lli\n", sum);
+	return (0);
+}
