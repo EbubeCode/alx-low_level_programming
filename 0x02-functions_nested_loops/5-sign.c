7db5a7ce39c83d8a0385bd01205a28efca5f42bc
EbubeCode <chukwuma258@gmail.com>
2022-03-15 14:09:00 +0100

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - prints the sign of n
- * @n - the number to determine its sign
+ * @n: the number to determine its sign
  *
  * Return: 0 if n = 0, 1 if n > 0, -1 if n < 0
  */
