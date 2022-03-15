39bf54e774a20cc849440f1532c2be88f1852407
EbubeCode <chukwuma258@gmail.com>
2022-03-15 14:45:11 +0100

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,48 @@
 #include "main.h"
 
 /**
+ * switch_print - use switch block to print a number
+ * @n: number to be printed
+ *
+ * Return: void
+ */
+void switch_print(int n)
+{
+
+	switch (n)
+	{
+	case 0:
+		_putchar('0');
+		break;
+	case 1:
+		_putchar('1');
+		break;
+	case 2:
+		_putchar('2');
+		break;
+	case 3:
+		_putchar('3');
+		break;
+	case 4:
+		_putchar('4');
+		break;
+	case 5:
+		_putchar('5');
+		break;
+	case 6:
+		_putchar('6');
+		break;
+	case 7:
+		_putchar('7');
+		break;
+	case 8:
+		_putchar('8');
+		break;
+	default:
+		_putchar('9');
+	}
+}
+/**
  * print_last_digit - prints the last digit of a number n
  * @n: number to print it's last digit
  *
@@ -12,37 +54,10 @@ int print_last_digit(int n)
 	int abs;
 
 	x = n % 10;
-	abs = _abs(x);
-	switch (abs) {
-		case 0:
-			_putchar('0');
-			break;
-		case 1:
-			_putchar('1');
-			break;
-		case 2:
-			_putchar('2');
-			break;
-		case 3:
-			_putchar('3');
-			break;
-		case 4:
-			_putchar('4');
-			break;
-		case 5:
-			_putchar('5');
-			break;
-		case 6:
-			_putchar('6');
-			break;
-		case 7:
-			_putchar('7');
-			break;
-		case 8:
-			_putchar('8');
-			break;
-		default:
-			_putchar('9');
-	}
+	if (x < 0)
+		abs = 0 - x;
+	else
+		abs = x;
+	switch_print(abs);
 	return (x);
 }
