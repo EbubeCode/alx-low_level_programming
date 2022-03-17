9cd8aef3708cc6098449030a76d09e9742138ece
EbubeCode <chukwuma258@gmail.com>
2022-03-17 09:53:36 +0100

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies two integers a and b
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
