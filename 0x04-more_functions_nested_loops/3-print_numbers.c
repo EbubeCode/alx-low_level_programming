f3b4bfe360cbe196db045df2d2d4c024637d429d
EbubeCode <chukwuma258@gmail.com>
2022-03-17 10:03:22 +0100

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+		_putchar(i++);
+	_putchar('\n');
+}
