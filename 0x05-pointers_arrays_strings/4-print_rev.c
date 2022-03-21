2b32e642e426993d8ca51a08847675e32e4e5715
EbubeCode <chukwuma258@gmail.com>
2022-03-21 12:20:09 +0100

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the string s in reverse to stdout
+ * @s: string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	for (; *s != '\0'; ++s)
+		i++;
+	for (; i >= 0; --i)
+	{
+		_putchar(*s);
+		--s;
+	}
+	_putchar('\n');
+}
