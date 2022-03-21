d394b449017ee60a0523e1e1585a91f9db382b1b
EbubeCode <chukwuma258@gmail.com>
2022-03-21 11:31:19 +0100

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - takes a pointer to an n and updates the value it points to 98
+ * @n: pointer to an integer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
