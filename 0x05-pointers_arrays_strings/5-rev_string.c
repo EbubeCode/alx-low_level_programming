76ad9ba65343a3c4f19d47e24a3046335c38bc74
EbubeCode <chukwuma258@gmail.com>
2022-03-21 14:16:04 +0100

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - reverses the string s
+ * @s: string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i;
+	char *a;
+	int x;
+	char temp;
+
+	i = 0;
+	a = s;
+	for (; *s != '\0'; ++s)
+		i++;
+	for (x = 0; x < i/2; x++)
+	{
+		--s;
+		temp = *s;
+		*s = a[x];
+		a[x] = temp;
+	}
+}
