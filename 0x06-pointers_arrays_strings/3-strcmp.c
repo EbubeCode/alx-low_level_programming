8daadffc25dca18af3b7b3f0c5f0672f489d6a55
EbubeCode <chukwuma258@gmail.com>
2022-03-23 17:33:01 +0100

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+/**
+ * _strcmp - compares two strings s1 and s2
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: s1 < s2 (-1), s1 == s2 (0), s1 > s2 (1)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	if (s1[i] != s2[i])
+		return (s1[i] - s2[i]);
+	return (0);
+}
