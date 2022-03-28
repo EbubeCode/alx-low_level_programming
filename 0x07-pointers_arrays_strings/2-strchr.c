b45e47d77911c0c31a5a27c44b9171125e43fa85
EbubeCode <chukwuma258@gmail.com>
2022-03-28 13:09:47 +0100

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+/**
+ * _strchr - locates a character c in the string s
+ * @c: character to locate
+ * @s: string to search
+ *
+ * Return: pointer to the first occurence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0';  s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return (s);
+}
