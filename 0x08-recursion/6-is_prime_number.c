6175361ed6528456c3fa7e086ec86c7881bbafb0
EbubeCode <chukwuma258@gmail.com>
2022-03-30 12:50:51 +0100

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+/**
+ * get_prime - check if x is prime
+ * @x: the number
+ * @y: the divisor
+ *
+ * Return: 1 if n is prime else 0
+ */
+int get_prime(int x, int y)
+{
+	if ((x / 2) <= y)
+		return (1);
+	if (x % y == 0)
+		return (0);
+	return (get_prime(x, (y + 1)));
+}
+
+/**
+ * is_prime_number - checks if n is prime
+ * @n: the integer
+ *
+ * Return: 1 if n is prime else 0
+ */
+int is_prime_number(int n)
+{
+	if (n < 0)
+		n = 0 - n;
+	if (n == 1)
+		return (0);
+	return (get_prime(n, 2));
+}
