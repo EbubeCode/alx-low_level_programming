e89a5cd61ac5627800aef93682a38b8d6feed9ab
EbubeCode <chukwuma258@gmail.com>
2022-04-01 13:32:10 +0100

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		for (i = 1; i < argc; i++)
 		{
 			n = atoi(argv[i]);
-			if (n <= 0 && *argv[i] != '0')
+			if (n <= 0)
 			{
 				printf("Error\n");
 				return (1);
