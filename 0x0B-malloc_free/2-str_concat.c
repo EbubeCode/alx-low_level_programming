80a0091a259e7364f027a8786621d69494198697
EbubeCode <chukwuma258@gmail.com>
2022-04-04 15:00:15 +0100

    task-2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer that points to a newly allocated space in memory
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i = 0, j = 0;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	s = s1;
+	if (s != NULL)
+		for (; *s != '\0'; s++)
+			i++;
+	s = s2;
+	if (s != NULL)
+		for (; *s != '\0'; s++)
+			i++;
+	s = malloc(sizeof(*s1) * (i++));
+	if (s == NULL)
+		return (NULL);
+	for (; *s1 != '\0'; s1++)
+	{
+		s[j] = *s1;
+		j++;
+	}
+	for (; *s2 != '\0'; s2++)
+	{
+		s[j] = *s2;
+		j++;
+	}
+	s[j] = '\0';
+	return (s);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -3,6 +3,7 @@
 #define MAIN_H
 
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 
 #endif /* MAIN_H */
