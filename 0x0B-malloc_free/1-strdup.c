9e7db68037ff66398acc3c6e80772ea1d4a7e0bd
EbubeCode <chukwuma258@gmail.com>
2022-04-04 14:37:36 +0100

    task-1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - copy the string str to another allocated memory
+ * @str: the string to copy
+ *
+ * Return: the pointer to a newly allocated space in memory
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	s = str;
+	for (i = 1; *s != '\0'; i++)
+		s++;
+	s = malloc(sizeof(*str) * (++i));
+	if (s == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+		s[j] = str[j];
+	return (s);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,6 +2,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 
 #endif /* MAIN_H */
