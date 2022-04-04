be7f2f08c6b24ea4413763297d8853f1dbe52141
EbubeCode <chukwuma258@gmail.com>
2022-04-04 14:19:40 +0100

    task-0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,8 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	arr = malloc(sizeof(*arr) * size);
 	if (arr == NULL)
 		return (NULL);
