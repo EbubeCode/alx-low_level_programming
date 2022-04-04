ba6639bb244ffb2fb40704bcff5540e1c98ed52c
EbubeCode <chukwuma258@gmail.com>
2022-04-04 15:46:59 +0100

    task-3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,10 +16,14 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	arr = (int **)malloc(sizeof(int *) * height);
-	for (i = 0; i < height; i++)
-		arr[i] = (int *)malloc(width * sizeof(int));
 	if (arr == NULL)
 		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *)malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+			return (NULL);
+	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
