4410ce5b46f898d2c352516d5cecbe18295668c1
EbubeCode <chukwuma258@gmail.com>
2022-04-04 16:04:26 +0100

    task-4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - free the memory allocated to the 2D array grid
+ * @grid: the 2D array
+ * @height: the number of rows
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	if (grid == NULL || height <= 0)
+		return;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -6,5 +6,7 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 char *create_array(unsigned int size, char c);
+void free_grid(int **grid, int height);
+
 
 #endif /* MAIN_H */
