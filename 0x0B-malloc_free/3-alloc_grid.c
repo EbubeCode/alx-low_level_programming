cc60c4a3c2e3969474c4ec0c4e54778c71aba6d9
EbubeCode <chukwuma258@gmail.com>
2022-04-04 15:43:26 +0100

    task-3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - create a 2 dimensional array of integers
+ * @width: the number of columns of the array
+ * @height: the number of rows of the array
+ *
+ * Return: pointer to the 2D array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	arr = (int **)malloc(sizeof(int *) * height);
+	for (i = 0; i < height; i++)
+		arr[i] = (int *)malloc(width * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	return (arr);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -4,6 +4,7 @@
 
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 char *create_array(unsigned int size, char c);
 
 #endif /* MAIN_H */
