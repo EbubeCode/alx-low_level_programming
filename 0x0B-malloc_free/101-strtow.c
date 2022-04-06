cfe3d9994fef9d057957f1d58cb65d4da179b849
EbubeCode <chukwuma258@gmail.com>
2022-04-07 00:15:49 +0100

    task-6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: an array of strings
+ */
+char **strtow(char *str)
+{
+	char **arr, *a = str;
+	int i = 1, stat = 0, n, j = 0;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	for (; *a != '\0'; a++)
+		if (*a != ' ' && stat == 0)
+		{
+			stat = 1;
+			i++;
+		}
+		else if (*a == ' ')
+			stat = 0;
+	arr = (char **)malloc(sizeof(char *) * i);
+	if (arr == NULL)
+		return (NULL);
+	for (a = str; *a != '\0'; a++)
+		if (*a != ' ')
+		{
+			for (n = 1; *a != ' ' && *a != '\0'; a++)
+				n++;
+			arr[j] = (char *)malloc(sizeof(char) * n);
+			if (arr[j++] == NULL)
+				return (NULL);
+		}
+	i = 0;
+	for (a = str; *a != '\0'; a++)
+		if (*a != ' ')
+		{
+			for (j = 0; *a != ' ' && *a != '\0'; a++)
+				arr[i][j++] = *a;
+			arr[i][j] = '\0';
+			i++;
+		}
+	return (arr);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -8,6 +8,6 @@ int **alloc_grid(int width, int height);
 char *create_array(unsigned int size, char c);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-
+char **strtow(char *str);
 
 #endif /* MAIN_H */
