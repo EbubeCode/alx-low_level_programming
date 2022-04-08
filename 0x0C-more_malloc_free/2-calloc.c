d5d3631859e1184bcda1ac7ca301a18f6a521204
EbubeCode <chukwuma258@gmail.com>
2022-04-08 11:21:59 +0100

    task-2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,6 +15,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	a = malloc(size * nmemb);
+	if (a == NULL)
+		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
 		a[i] = 0;
 	return (a);
