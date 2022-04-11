9cbd7744a965f730ffd0bda2963d64a4be5c639f
EbubeCode <chukwuma258@gmail.com>
2022-04-11 22:19:38 +0100

    task-0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef D_MACROS
+#define D_MACROS
+
+/**
+ * struct dog - defines a the type dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* end ifndef D_MACROS */
