1608e1ea984b6378b91db9a558a16a958bb1d53f
EbubeCode <chukwuma258@gmail.com>
2022-04-13 14:34:01 +0100

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 /**
  * print_name - prints a name
  * @name: name to be printed
@@ -7,5 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
 }
