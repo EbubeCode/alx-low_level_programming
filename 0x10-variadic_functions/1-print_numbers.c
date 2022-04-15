7541c227a8d7e74fa4689b3a4f4bf9a6294ed489
EbubeCode <chukwuma258@gmail.com>
2022-04-15 03:13:05 +0100

    task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i == (n - 1))
 			printf("%d", va_arg(list, int));
 		else
-			printf("%d %s", va_arg(list, int), separator);
+			printf("%d%s ", va_arg(list, int), separator);
 	}
 	va_end(list);
 	printf("\n");
