32eeb9c83e5dac9ff70e989650ce829b2b35c374
EbubeCode <chukwuma258@gmail.com>
2022-04-15 08:53:38 +0100

    task3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
