7ea0174b9664e83ee0a5c8c0bf9414cff2efe9a3
EbubeCode <chukwuma258@gmail.com>
2022-04-15 03:16:20 +0100

    task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int i;
 
-	if (separator == NULL)
+	if (separator == NULL || n == 0)
 		return;
 	va_start(list, n);
 	for (i = 0; i < n; i++)
