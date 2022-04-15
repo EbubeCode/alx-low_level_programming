4eb32be40beadcb10f7871c9a173b624ac456e24
EbubeCode <chukwuma258@gmail.com>
2022-04-15 03:00:53 +0100

    task1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_numbers - prints numbers provided
+ * @separator: seperator between numbers
+ * @n: the number of variable parameters
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	if (n == 0 || separator == NULL)
+		return;
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+			printf("%d", va_arg(list, int));
+		else
+			printf("%d %s", va_arg(list, int), separator);
+	}
+	va_end(list);
+	printf("\n");
+}
