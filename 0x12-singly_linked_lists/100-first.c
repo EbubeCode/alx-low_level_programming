562b7e0423d2011e729d8aa03074e1f07e42feca
EbubeCode <chukwuma258@gmail.com>
2022-04-21 14:29:57 +0100

    task5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * my_startup - prints before main
+ *
+ * Return: void
+ */
+void my_startup(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
