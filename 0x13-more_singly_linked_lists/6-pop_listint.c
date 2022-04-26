d230157d8ff931081ebb5231188c9822a624d085
EbubeCode <chukwuma258@gmail.com>
2022-04-26 16:26:45 +0100

    0x13 task6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete the head node of a listint_t
+ * @head: pointer to the head of the list
+ *
+ * Return: node's data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (head == NULL)
+		return (0);
+
+	temp = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = temp;
+
+	return (n);
+}
