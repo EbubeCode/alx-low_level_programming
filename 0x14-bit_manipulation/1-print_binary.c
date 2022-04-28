feaba30c6e1c7318b75c951b2b7484a275651887
EbubeCode <chukwuma258@gmail.com>
2022-04-28 15:09:36 +0100

    0x14 task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int b = 0x1000000000000000;
+	unsigned long int a = n & b;
+	int i = 3;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (a != b)
+	{
+		n = n << 1;
+		a = n & b;
+		i++;
+	}
+	i = 64 - i;
+	while (i > 0)
+	{
+		a = n & b;
+		if (a == 0)
+			_putchar('0');
+		else
+			_putchar('1');
+		n = n << 1;
+		i--;
+	}
+}
