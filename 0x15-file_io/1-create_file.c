65cbca62a4026dc9a9b8893bc6b42053b4bd6042
EbubeCode <chukwuma258@gmail.com>
2022-05-04 19:04:41 +0100

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	while (text_content[size] != '\0')
 		size++;
 	if (size > 0)
