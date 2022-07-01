000a8f6d6e52f98494164dc3a22d6f185ca19a4b
EbubeCode <chukwuma258@gmail.com>
2022-07-01 19:24:04 +0100

    0x1A task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key of the element
+ * @value: value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht,
+				const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *temp = NULL, *index_node;
+
+	if (ht == NULL || strlen(key) == 0)
+		return (0);
+	temp = malloc(sizeof(hash_node_t));
+
+	if (temp == NULL)
+		return (0);
+	temp->key = (char *)key;
+	temp->value = (char *)value;
+	temp->next = NULL;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	index_node = ht->array[index];
+
+	if (index_node != NULL)
+		temp->next = index_node;
+	ht->array[index] = temp;
+
+	return (0);
+}
