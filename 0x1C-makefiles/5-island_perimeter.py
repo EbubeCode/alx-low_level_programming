b780c4a43c6958f480c2036faedd94c6ad8dec2d
EbubeCode <chukwuma258@gmail.com>
2022-08-18 06:20:55 +0100

    on task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,18 +6,22 @@ def island_perimeter(grid):
     perim = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
+            tmp = 0
             if i != 0:
                 if check_grid(grid, i, j, i - 1, j):
-                    perim += 1
+                    tmp += 1
             if i != len(grid) - 1:
                 if check_grid(grid, i, j, i + 1, j):
-                    perim += 1
+                    tmp += 1
             if j != 0:
                 if check_grid(grid, i, j, i, j - 1):
-                    perim += 1
+                    tmp += 1
             if j != len(grid[i]) - 1:
                 if check_grid(grid, i, j, i, j + 1):
-                    perim += 1
+                    tmp += 1
+            if tmp == 4:
+                return 0
+            perim += tmp
 
     return perim
 
