5c280632afd917624c6442089ccb585e35ace9c5
EbubeCode <chukwuma258@gmail.com>
2022-08-18 06:15:04 +0100

    on task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,24 +3,23 @@
 
 def island_perimeter(grid):
     """island perimeter"""
-    if grid is not None and type(grid) == list:
-        perim = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if i != 0:
-                    if check_grid(grid, i, j, i - 1, j):
-                        perim += 1
-                if i != len(grid) - 1:
-                    if check_grid(grid, i, j, i + 1, j):
-                        perim += 1
-                if j != 0:
-                    if check_grid(grid, i, j, i, j - 1):
-                        perim += 1
-                if j != len(grid[i]) - 1:
-                    if check_grid(grid, i, j, i, j + 1):
-                        perim += 1
+    perim = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if i != 0:
+                if check_grid(grid, i, j, i - 1, j):
+                    perim += 1
+            if i != len(grid) - 1:
+                if check_grid(grid, i, j, i + 1, j):
+                    perim += 1
+            if j != 0:
+                if check_grid(grid, i, j, i, j - 1):
+                    perim += 1
+            if j != len(grid[i]) - 1:
+                if check_grid(grid, i, j, i, j + 1):
+                    perim += 1
 
-        return perim
+    return perim
 
 def check_grid(grid, i, j, x, y):
     """check grid"""
